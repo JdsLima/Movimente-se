a93231848830b53fca9e713b6e545b3be7da2d48
Jadson <jadsonmath874@gmail.com>
2021-03-03 20:31:27 -0400

    melhorias visuais

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Cookies from 'js-cookie';
 import styles from '../styles/components/Login.module.css';
+import Head from 'next/head';
 
 export function Login() {
     function getUser() {
@@ -13,24 +14,29 @@ export function Login() {
     }
 
     return (
-        <div className={styles.LoginContainer}>
-            <div className={styles.logo}>
-                <img src="favicon-sem-fundo.png" alt="" />
-                <strong>Movimente<span>-</span>se</strong>
+        <>
+            <Head>
+                <title>Movimente-se</title>
+            </Head>
+            <div className={styles.LoginContainer}>
+                <div className={styles.logo}>
+                    <img src="favicon-sem-fundo.png" alt="" />
+                    <strong>Movimente<span>-</span>se</strong>
+                </div>
+                <p>Bem-vindo</p>
+                <div className={styles.msg}>
+                    <img src="/icons/GitHub.png" alt="Github" />
+                    <strong>Faça login com seu Github para começar.</strong>
+                </div>
+                <div className={styles.inputContainer}>
+                    <input type="text" id="userName" placeholder="Digite seu username" />
+                    <Link href="/inicio">
+                        <button type='button' id="botaoUser" onClick={getUser}>
+                            <img src="icons/seta-direita.png" alt="avançar" />
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <p>Bem-vindo</p>
-            <div className={styles.msg}>
-                <img src="/icons/GitHub.png" alt="Github" />
-                <strong>Faça login com seu Github para começar.</strong>
-            </div>
-            <div className={styles.inputContainer}>
-                <input type="text" id="userName" placeholder="Digite seu username" />
-                <Link href="/inicio">
-                    <button type='button' id="botaoUser" onClick={getUser}>
-                        <img src="icons/seta-direita.png" alt="avançar" />
-                    </button>
-                </Link>
-            </div>
-        </div>
+        </>
     );
 }
